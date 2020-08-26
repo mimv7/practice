c189995877be74c7e2357c9e835f830dad6e7f4a
Meleshin Roman <meleshin.r@yandex.ru>
2020-08-26 16:22:23 +0300

    верстка портфолио

--- go-studio/time.js
+++ go-studio/time.js
@@ -1,18 +1,21 @@
-timeWork();
 
-function timeWork() {
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
-if (hourNow > 19) {
-greeting = 'К сожалению Мы закрыты. Перезвоните с утра!';
+time();
+function time() {
+    var today = new Date();
+    var hourNow = today.getHours();
+    var greeting;
+    if (hourNow > 19) {
+        greeting = 'К сожалению Мы закрыты. Перезвоните с утра!';
+    }
+    else if (hourNow > 11) {
+        greeting = 'Добрый день!';
+    }
+    else if (hourNow > 7) {
+        greeting = 'Доброе утро!';
+    }
+    else {
+        greeting = 'К сожалению Мы закрыты. Перезвоните с утра!';
+    }
+    document.write('<h3>' + greeting + '</h3>');
 }
-else if (hourNow > 11){
-greeting = 'Добрый день!';
-}else if(hourNow> 7 ){
-greeting='Доброе утро!';
-}else{
-greeting='К сожалению Мы закрыты. Перезвоните с утра!';
-}
-document.write('<h3>' + greeting + '</h3>');
-}
+
