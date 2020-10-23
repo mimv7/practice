01e5f1b4ab0d3241e9a457d6fb32a9bd2b8cca6a
Meleshin Roman <meleshin.r@yandex.ru>
2020-10-23 19:15:13 +0300

    -

---
+++ go-studio/js/burger.js
@@ -0,0 +1,5 @@
+$(document).ready(function burger(){
+    $('.burger__menu').click(function(){
+        $('.navigation__menu').toggle();
+    });
+});
---
+++ go-studio/js/index.js
@@ -0,0 +1,4 @@
+let msg='ошибка';
+let errorMsg=document.getElementsByClassName('.contacts--form__form');
+errorMsg.textContent=msg;
+document.getElementsByClassName('.contacts--form__form').delite;
