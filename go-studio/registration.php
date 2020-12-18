3767e1f118ad5e7c6cbdc0240ff1b22d25d9731f
Meleshin Roman <meleshin.r@yandex.ru>
2020-12-18 20:10:31 +0300

    форма обратной связи

--- go-studio/registration.php
+++ go-studio/registration.php
@@ -1,3 +1,18 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $subject = 'запись'ж
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $date = $_POST['date'];
+    $msg = клиент . $name .  '(' . $phone . ')' . 'записался на ' .$data; 
+    mail('meleshin.r@yandex.ru', $subject, $msg);
+    ?>
+</body>
+</html>
