28b1e2eaced5961d69af9bd9314c461065729860
Meleshin Roman <meleshin.r@yandex.ru>
2020-12-25 19:17:40 +0300

    добавленно тестовое задание

--- go-studio/registration.php
+++ go-studio/registration.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    $subject = 'запись'ж
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $date = $_POST['date'];
-    $msg = клиент . $name .  '(' . $phone . ')' . 'записался на ' .$data; 
-    mail('meleshin.r@yandex.ru', $subject, $msg);
-    ?>
-</body>
-</html>
+for($score = 10 ; $score >= 0; $score-- ){
+    echo '<br>you score : ' .  $score/2;
+}
